3e9b2816994f6c94d8fbeff94872cf90aab141fd
WINDOWS-SERVER-\kiyokazu180568 <kiyokazu@ishida.co.jp>
2023-05-21 12:05:55 +0900

    ログイン＆新規会員登録画面実装

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../shared/auth.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-register',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
     test : Date = new Date();
-    focus: any;
-    focus1: any;
-    constructor() { }
+    errors : any=[]
+    constructor(
+        private authService: AuthService,
+        private router: Router
+        ) { }
 
     ngOnInit() {}
+
+    register(registerForm: { value: string; }){
+        this.authService.register(registerForm.value).subscribe(
+            (result) => { 
+                console.log("Success!")
+                this.router.navigate(['/login'])
+            },
+            (err: HttpErrorResponse) => { 
+                console.error(err)
+                this.errors = err.error.errors
+            }
+        )
+    }
 }
